856d38335d624524e5182da6b7ad62c62883809e
Michael <michaelcaxias.dev@gmail.com>
2021-10-28 19:59:15 -0300

    Req 1: Completo

---
+++ src/components/Table.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+const tableHeaders = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate',
+  'Gravity', 'Terrain', 'Surface Water',
+  'Population', 'Films', 'Created', 'Edited', 'URL'];
+
+export default function Table() {
+  const [data, setData] = useState([]);
+
+  const fetchPlanets = async () => {
+    const response = await fetch(URL);
+    const resolve = await response.json();
+    setData(resolve.results);
+  };
+
+  useEffect(() => {
+    fetchPlanets();
+  }, [data]);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          { tableHeaders.map((header) => (
+            <th key={ header }>{header}</th>
+          )) }
+        </tr>
+      </thead>
+      {/* { map abaixo feito com ajuda do github copilot } */}
+      <tbody>
+        { data.map((planet) => (
+          <tr key={ planet.url }>
+            <td>{ planet.name }</td>
+            <td>{ planet.rotation_period }</td>
+            <td>{ planet.orbital_period }</td>
+            <td>{ planet.diameter }</td>
+            <td>{ planet.climate }</td>
+            <td>{ planet.gravity }</td>
+            <td>{ planet.terrain }</td>
+            <td>{ planet.surface_water }</td>
+            <td>{ planet.population }</td>
+            <td>{ planet.films.length }</td>
+            <td>{ planet.created }</td>
+            <td>{ planet.edited }</td>
+            <td>{ planet.url }</td>
+          </tr>
+        )) }
+      </tbody>
+    </table>
+  );
+}
