5808c75453819a86a2439b076865ef04314e4ce6
DiogoFiuza <difipalm@gmail.com>
2021-11-01 17:07:16 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import AppContext from './Context';
+import Table from './Table';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const api = async () => {
+    const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+    const obj = await response.json();
+    const result = obj.results;
+    setData(result);
+    return result;
+  };
+
+  const contextValue = {
+    data,
+    setData,
+  };
+
+  useEffect(() => {
+    api();
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    <AppContext.Provider value={ contextValue }>
+      <Table />
+    </AppContext.Provider>
   );
 }
 
---
+++ src/Table.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import AppContext from './Context';
+
+function Table() {
+  const { data } = useContext(AppContext);
+  console.log(data);
+
+  const tableHead = ['Name', 'Rotation Period', 'Orbital Period',
+    'Diameter', 'Climate', 'Gravity', 'Terrain', 'Surface Water',
+    'Population', 'Films', 'Create', 'Edited', 'Url'];
+
+  return (
+    <table align="center">
+      <tbody>
+        <tr>
+          {tableHead.map((element, index) => (
+            <th key={ index }><b>{ element }</b></th>
+          ))}
+        </tr>
+        {data.map((element, index) => (
+          <tr key={ index }>
+            <td>{ element.name }</td>
+            <td>{ element.rotation_period }</td>
+            <td>{ element.orbital_period }</td>
+            <td>{ element.diameter }</td>
+            <td>{ element.climate }</td>
+            <td>{ element.gravity }</td>
+            <td>{ element.terrain }</td>
+            <td>{ element.surface_water }</td>
+            <td>{ element.population }</td>
+            <td><a href=" element.films">{ element.films }</a></td>
+            <td>{ element.created }</td>
+            <td>{ element.edited }</td>
+            <td><a href="element.url">{ element.url }</a></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
