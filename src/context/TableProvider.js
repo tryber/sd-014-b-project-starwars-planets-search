f48201bacd4670581628609c895c736270cf490a
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-11-01 12:58:25 -0300

    requisito 1

--- src/context/TableProvider.js
+++ src/context/TableProvider.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import TableContext from './TableContext';
-import getPlanets from '../services/starWarsApi';
 
-async function TableProvider({ children }) {
-  const data = await getPlanets();
+function TableProvider({ children }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchPlanets = async () => {
+      const response = await fetch(URL);
+      const resolve = await response.json();
+      setData(resolve.results);
+      setLoading(false);
+    };
+    fetchPlanets();
+  }, []);
 
   return (
-    <TableContext.Provider value={ data }>
+    <TableContext.Provider value={ { data, loading } }>
       {children}
     </TableContext.Provider>
   );
 }
+
+TableProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export default TableProvider;
