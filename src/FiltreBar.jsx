4ff001c1c81132101d6297eeaa031e9281ee5d25
DiogoFiuza <difipalm@gmail.com>
2021-11-02 14:02:49 -0300

    Fix lint problems

--- src/FiltreBar.jsx
+++ src/FiltreBar.jsx
@@ -14,9 +14,15 @@ function FilterBar() {
 
   const handleClick = () => {
     const filtred = data.filter((planet) => {
-      if (compairsonFilter === 'maior que') return Number(planet[columnFilter]) > Number(value);
-      if (compairsonFilter === 'menor que') return Number(planet[columnFilter]) < Number(value);
-      if (compairsonFilter === 'igual a') return Number(planet[columnFilter]) === Number(value);
+      if (compairsonFilter === 'maior que') {
+        return Number(planet[columnFilter]) > Number(value);
+      }
+      if (compairsonFilter === 'menor que') {
+        return Number(planet[columnFilter]) < Number(value);
+      }
+      if (compairsonFilter === 'igual a') {
+        return Number(planet[columnFilter]) === Number(value);
+      }
       return null;
     });
     setFiltred(filtred);
@@ -24,19 +30,29 @@ function FilterBar() {
 
   return (
     <>
-      <select onChange={ (event) => setColumnFilter(event.target.value) } data-testid="column-filter">
+      <select
+        onChange={ (event) => setColumnFilter(event.target.value) }
+        data-testid="column-filter"
+      >
         <option value="population">population</option>
         <option value="orbital_period">orbital_period</option>
         <option value="diameter">diameter</option>
         <option value="rotation_period">rotation_period</option>
         <option value="surface_water">surface_water</option>
       </select>
-      <select onChange={ (event) => setComparisonFilter(event.target.value) } data-testid="comparison-filter">
+      <select
+        onChange={ (event) => setComparisonFilter(event.target.value) }
+        data-testid="comparison-filter"
+      >
         <option value="maior que">maior que</option>
         <option value="menor que">menor que</option>
         <option value="igual a">igual a</option>
       </select>
-      <input onChange={ (event) => setValue(event.target.value) } type="number" data-testid="value-filter" />
+      <input
+        onChange={ (event) => setValue(event.target.value) }
+        type="number"
+        data-testid="value-filter"
+      />
       <button
         type="button"
         data-testid="button-filter"
