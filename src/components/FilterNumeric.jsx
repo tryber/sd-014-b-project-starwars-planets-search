027f5df9c233a5effcb4a676ac2491477e6eb575
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-11-02 14:02:04 -0300

    Requisito 4 concluido

--- src/components/FilterNumeric.jsx
+++ src/components/FilterNumeric.jsx
@@ -7,9 +7,12 @@ function FilterNumeric() {
     comparison: 'maior que',
     value: '0',
   };
+  const initialColumnOptions = ['population', 'orbital_period', 'diameter',
+    'rotation_period', 'surface_water'];
 
-  const { handleFilterValues } = useContext(Context);
+  const { handleFilterValues, filters: { filterByNumericValues } } = useContext(Context);
   const [filterNumeric, setFilterNumeric] = useState(initialFilter);
+  const [columnOptions, setColumnOptions] = useState(initialColumnOptions);
 
   const handleChange = ({ target }) => {
     setFilterNumeric({
@@ -18,14 +21,22 @@ function FilterNumeric() {
     });
   };
 
+  const submitFilterValues = () => {
+    handleFilterValues(filterNumeric);
+    const newOptions = [...columnOptions];
+    newOptions.splice(columnOptions.indexOf(filterNumeric.column), 1);
+    console.log(newOptions);
+    setColumnOptions(newOptions);
+  };
+
   return (
     <section>
       <select data-testid="column-filter" name="column" onChange={ handleChange }>
-        <option value="population">population</option>
-        <option value="orbital_period">orbital_period</option>
-        <option value="diameter">diameter</option>
-        <option value="rotation_period">rotation_period</option>
-        <option value="surface_water">surface_water</option>
+        {
+          columnOptions.map((option) => (
+            <option value={ option } key={ option }>{ option }</option>
+          ))
+        }
       </select>
 
       <select data-testid="comparison-filter" name="comparison" onChange={ handleChange }>
@@ -44,10 +55,21 @@ function FilterNumeric() {
       <button
         type="button"
         data-testid="button-filter"
-        onClick={ () => handleFilterValues(filterNumeric) }
+        onClick={ submitFilterValues }
       >
         Filtrar
       </button>
+
+      <section>
+        {
+          filterByNumericValues.map(({ column, comparison, value }, i) => (
+            <div key={ i } data-testid="filter">
+              { `${column} ${comparison} ${value}` }
+              <button type="button">X</button>
+            </div>
+          ))
+        }
+      </section>
     </section>
   );
 }
