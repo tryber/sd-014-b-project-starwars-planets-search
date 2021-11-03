f731757d5d564b83cfaf02a119e95f83b0524aed
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-11-03 17:41:39 -0300

    1 - Correçao do lint nome das chaves do data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -24,26 +24,28 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {data.map(({
-            name, rotation_period, orbital_period, diameter, climate,
-            gravity, terrain, surface_water, population, films,
-            created, edited, url,
-          }, index) => (
-            <tr key={ index }>
-              <td key={ name }>{name}</td>
-              <td key={ rotation_period }>{rotation_period}</td>
-              <td key={ orbital_period }>{orbital_period}</td>
-              <td key={ diameter }>{diameter}</td>
-              <td key={ climate }>{climate}</td>
-              <td key={ gravity }>{gravity}</td>
-              <td key={ terrain }>{terrain}</td>
-              <td key={ surface_water }>{surface_water}</td>
-              <td key={ population }>{population}</td>
-              <td key={ films }>{films}</td>
-              <td key={ created }>{created}</td>
-              <td key={ edited }>{edited}</td>
-              <td key={ url }>{url}</td>
-            </tr>))}
+          {data.map((planet, index) => {
+            const { name, diameter, climate, gravity, terrain,
+              population, films, created, edited, url } = planet;
+
+            return (
+              <tr key={ index }>
+                <td key={ name }>{name}</td>
+                <td key={ planet.rotation_period }>{planet.rotation_period}</td>
+                <td key={ planet.orbital_period }>{planet.orbital_period}</td>
+                <td key={ diameter }>{diameter}</td>
+                <td key={ climate }>{climate}</td>
+                <td key={ gravity }>{gravity}</td>
+                <td key={ terrain }>{terrain}</td>
+                <td key={ planet.surface_water }>{planet.surface_water}</td>
+                <td key={ population }>{population}</td>
+                <td key={ films }>{films}</td>
+                <td key={ created }>{created}</td>
+                <td key={ edited }>{edited}</td>
+                <td key={ url }>{url}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     );
