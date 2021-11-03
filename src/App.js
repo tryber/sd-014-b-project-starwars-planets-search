5d7bee193166a0d1ed53dc1671f277febb02206a
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-11-03 16:36:16 -0300

    Requisito 1 ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import MyComponent from './context-api/Provider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <MyComponent>
+      <span>Hello, App!</span>
+      <Table />
+    </MyComponent>
   );
 }
 
---
+++ src/context-api/Provider.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import MyContext from './MyContext';
+import getPlanets from '../services/getPlanets';
+
+function Provider({ children }) {
+  const [planets, setPlanets] = useState([]);
+
+  const context = {
+    data: planets,
+
+  };
+
+  const fetchPlanets = async () => {
+    const result = await getPlanets();
+    // console.log(result);
+    setPlanets(result);
+  };
+
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
+
+  return (
+    <MyContext.Provider value={ context }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
