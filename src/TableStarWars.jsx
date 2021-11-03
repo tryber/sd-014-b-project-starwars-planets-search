b22659ef682d96a34efc65b7a7978246792a49b7
Vitor Silva <vitor.o.silva95@gmail.com>
2021-11-03 17:27:32 -0300

    Requisito 3

--- src/TableStarWars.jsx
+++ src/TableStarWars.jsx
@@ -1,23 +1,95 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import StarWarsContext from './context/StarWarsContext';
+import Select from './components/Select';
+
+const columns = ['population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water'];
+const valueRange = ['maior que', 'menor que', 'igual a'];
 
 function TableStarWars() {
-  const { data, keys, setPlanetFilter } = useContext(StarWarsContext);
-  const [filterPlanet, setPlanet] = useState('');
+  const {
+    data,
+    keys,
+    inputName,
+    setInputName,
+    filters,
+    setColumn,
+    setComparison,
+    setValue,
+    setData,
+  } = useContext(StarWarsContext);
 
-  useEffect(() => {
-    setPlanetFilter(filterPlanet);
-    console.log('chamou');
-  }, [filterPlanet, setPlanetFilter]);
+  function handleChange(event) {
+    setInputName(event.target.value);
+  }
+
+  function handleColumns(event) {
+    setColumn(event.target.value);
+  }
+
+  function handleComparison(event) {
+    setComparison(event.target.value);
+  }
+
+  function handleValue(event) {
+    setValue(event.target.value);
+  }
+
+  const { filterByName: { name } } = filters;
+  const { filterByNumericValues: [{ column, comparison, value }] } = filters;
+
+  function handleClick() {
+    if (comparison === 'maior que') {
+      const filtered = data.filter((planet) => Number(planet[column]) > value);
+      setData(filtered);
+    }
+    if (comparison === 'menor que') {
+      const filtered = data.filter((planet) => Number(planet[column]) < value);
+      setData(filtered);
+    }
+    if (comparison === 'igual a') {
+      const filtered = data.filter((planet) => planet[column] === value);
+      setData(filtered);
+    }
+  }
 
   return (
     <div>
       <input
         type="text"
-        value={ filterPlanet }
+        value={ inputName }
         data-testid="name-filter"
-        onChange={ (e) => setPlanet(e.target.value) }
+        onChange={ handleChange }
       />
+      <Select
+        array={ columns }
+        value={ column }
+        dataTestID="column-filter"
+        onChange={ handleColumns }
+      />
+      <Select
+        array={ valueRange }
+        value={ comparison }
+        onChange={ handleComparison }
+        dataTestID="comparison-filter"
+      />
+      <input
+        type="number"
+        onChange={ handleValue }
+        value={ value }
+        data-testid="value-filter"
+      />
+      <button
+        type="submit"
+        onClick={ handleClick }
+        data-testid="button-filter"
+      >
+        Filtrar
+
+      </button>
       <table>
         <thead>
           <tr>
@@ -25,7 +97,7 @@ function TableStarWars() {
           </tr>
         </thead>
         <tbody>
-          {data.filter((planet) => planet.name.includes(filterPlanet))
+          {data.filter((planet) => planet.name.includes(name))
             .map((element, index) => (
               <tr key={ index }>
                 <td>{element.name}</td>
--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -6,30 +6,44 @@ import requestApi from '../services/api';
 function Provider({ children }) {
   const [dataResult, setData] = useState([]);
   const [keys, setKeys] = useState([]);
-  const [inputName, setInputName] = useState();
+  const [inputName, setInputName] = useState('');
+  const [column, setColumn] = useState('diameter');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState();
 
   async function setApi() {
     const result = await requestApi();
     const data = result.results;
-    const resultData = data.map(({ residents, ...value }) => value);
+    const resultData = data.map(({ residents, ...element }) => element);
     setData(resultData);
     setKeys(Object.keys(resultData[0]));
+    return resultData;
   }
   useEffect(() => { setApi(); }, []);
-
-  function setPlanetFilter(inputValue) {
-    setInputName(inputValue);
-  }
 
   const filters = {
     filterByName: {
       name: inputName,
     },
+    filterByNumericValues: [
+      { column,
+        comparison,
+        value,
+      },
+    ],
   };
 
   return (
     <StarWarsContext.Provider
-      value={ { data: dataResult, keys, filters, setPlanetFilter } }
+      value={ { data: dataResult,
+        keys,
+        filters,
+        setInputName,
+        setColumn,
+        setComparison,
+        setValue,
+        setData,
+      } }
     >
       { children }
     </StarWarsContext.Provider>
