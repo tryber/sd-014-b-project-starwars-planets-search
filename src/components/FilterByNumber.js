e908a8ab52f3eb7c3159fa1dd495faaa719d11b0
LucasAccurcio <lucas.accurcio@gmail.com>
2021-11-03 19:41:32 -0300

    Requesito 03 em implementaçao

--- src/components/FilterByNumber.js
+++ src/components/FilterByNumber.js
@@ -1,31 +1,59 @@
 import React, { useContext } from 'react';
 import Context from '../context/Context';
 
+let obj = {
+  column: 'population',
+  comparison: 'maior que',
+  value: '0',
+};
+
 function FilterByNumber() {
-  const { filterByNumber, filtersToSelect } = useContext(Context);
+  const {
+    setAuxFilterByNumber,
+    auxFilterByNumber,
+    setFiltersByNumber,
+    filtersToSelect,
+  } = useContext(Context);
+
+  const handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    obj = {
+      ...obj,
+      [name]: value,
+    };
+    setAuxFilterByNumber(obj);
+  };
+
+  const handleSubmit = () => {
+    setFiltersByNumber(auxFilterByNumber);
+  };
+
   return (
     <div>
-      <select data-testid="column-filter">
+      <select data-testid="column-filter" name="column" onChange={ handleChange }>
         {
           filtersToSelect.map((category, index) => (
             <option key={ index }>{category}</option>
           ))
         }
       </select>
-      <select data-testid="comparison-filter">
+      <select data-testid="comparison-filter" name="comparison" onChange={ handleChange }>
         <option>maior que</option>
         <option>menor que</option>
         <option>igual a</option>
       </select>
       <input
         data-testid="value-filter"
+        name="value"
         type="number"
         min="0"
+        onChange={ handleChange }
       />
       <button
         data-testid="button-filter"
         type="button"
-        onClick={ filterByNumber }
+        onClick={ handleSubmit }
       >
         Filtrar
       </button>
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -8,6 +8,7 @@ const INITIAL_FILTER = {
     filterByName: {
       name: '',
     },
+    filterByNumericValues: [],
   },
 };
 
@@ -20,10 +21,21 @@ const filtersToSelect = [
 ];
 
 const Provider = ({ children }) => {
+  // Dados originais
   const [originalList, setOriginalList] = useState([]);
+
+  // Dados que renderizaram a tabela
   const [planetList, setPlanetList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [filters, setFilters] = useState(INITIAL_FILTER);
+
+  const [tableFilters, setTableFilters] = useState(INITIAL_FILTER);
+
+  const [auxFilterByNumber, setAuxFilterByNumber] = useState({});
+  const [filtersByNumber, setFiltersByNumber] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: '0',
+  });
 
   const recoverPlanetList = async () => {
     const list = await getPlanetList();
@@ -36,11 +48,58 @@ const Provider = ({ children }) => {
     if (value === '') {
       setPlanetList(originalList);
     } else {
+      const { filters: { filterByNumericValues } } = tableFilters;
       const searchPlanet = planetList.filter((planet) => planet.name.toLowerCase()
         .includes(value.toLowerCase()));
-      setFilters({ ...filters, name: value });
+      setTableFilters({
+        ...tableFilters,
+        filters: {
+          filtersByName: { name: value },
+          filterByNumericValues,
+        },
+      });
       setPlanetList([...searchPlanet]);
     }
+  };
+
+  const filterByNumber = () => {
+    const { filters: { filterByNumericValues } } = tableFilters;
+    if (filtersByNumber.value === '0') return false;
+    filterByNumericValues.push(filtersByNumber);
+    setTableFilters({
+      ...tableFilters,
+      filters: {
+        filterByNumericValues,
+      },
+    });
+    console.log(planetList);
+    let newFilter = [];
+    filterByNumericValues.forEach((filtro) => {
+      newFilter = planetList.filter((planet) => {
+        if (filtro.comparison === 'maior que') {
+          if (planet[filtro.column] === 'unknown') {
+            return planet;
+          }
+          if (parseInt(planet[filtro.column], 10) > filtro.value) {
+            return planet;
+          }
+        }
+        if (filtro.comparison === 'menor que') {
+          if (planet[filtro.column] === 'unknown') {
+            return planet;
+          }
+          if (parseInt(planet[filtro.column], 10) < filtro.value) {
+            return planet;
+          }
+        }
+        return false;
+      });
+    });
+    console.log(newFilter);
+    /*     const searchPlanet = planetList.filter((planet) => {
+      planet.filtersByNumber.column
+    }); */
+    // console.log(searchPlanet);
   };
 
   const context = {
@@ -50,11 +109,20 @@ const Provider = ({ children }) => {
     setIsLoading,
     filterByName,
     filtersToSelect,
+    filtersByNumber,
+    setFiltersByNumber,
+    auxFilterByNumber,
+    setAuxFilterByNumber,
   };
 
   useEffect(() => {
     recoverPlanetList();
   }, []);
+
+  useEffect(() => {
+    filterByNumber();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filtersByNumber]);
 
   return (
     <Context.Provider value={ context }>
