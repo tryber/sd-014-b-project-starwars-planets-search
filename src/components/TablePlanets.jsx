1fb885b82bf66cf343ce2ccbf1e649070700a3d7
Jéssica Queiroz <gnr0034@gmail.com>
2021-11-03 18:49:39 -0300

    implementa toLowerCase method no filtro

--- src/components/TablePlanets.jsx
+++ src/components/TablePlanets.jsx
@@ -39,24 +39,25 @@ export default function TablePlanets() {
           <th>URL</th>
         </tr>
         <tbody>
-          {data.filter(({ name }) => inputText === '' || name.includes(inputText))
-            .map((planet) => (
-              <tr key={ planet.name }>
-                <td>{planet.name}</td>
-                <td>{planet.rotation_period}</td>
-                <td>{planet.orbital_period}</td>
-                <td>{planet.diameter}</td>
-                <td>{planet.climate}</td>
-                <td>{planet.gravity}</td>
-                <td>{planet.terrain}</td>
-                <td>{planet.surface_water}</td>
-                <td>{planet.population}</td>
-                <td>{planet.films}</td>
-                <td>{planet.created}</td>
-                <td>{planet.edited}</td>
-                <td>{planet.url}</td>
-              </tr>
-            ))}
+          {data.filter(({ name }) => (
+            inputText === '' || name.toLowerCase().includes(inputText)
+          )).map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
