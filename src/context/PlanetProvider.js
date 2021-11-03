5c2be9f836c658653ce925f3c3ec11d5e3e4301b
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-11-03 19:12:59 +0000

    configura filter no provider

--- src/context/PlanetProvider.js
+++ src/context/PlanetProvider.js
@@ -5,18 +5,38 @@ import getPlanet from '../services/planetApi';
 
 function PlanetProvider({ children }) {
   const [planet, setPlanet] = useState([]);
+  const [inicialPlanet, setInicialPlanet] = useState([]);
+  const [filterName, setFilterName] = useState('');
+  const [filter, setFilter] = useState({
+    filters: {
+      filterByName: {
+        name: filterName,
+      },
+    },
+  });
 
   async function requestApiPlanet() {
     const results = await getPlanet();
     setPlanet(results);
+    setInicialPlanet(results);
   }
 
   useEffect(() => {
     requestApiPlanet();
   }, []);
 
+  const contextValue = {
+    planet,
+    setPlanet,
+    filterName,
+    setFilterName,
+    filter,
+    setFilter,
+    inicialPlanet,
+  };
+
   return (
-    <PlanetContext.Provider value={ { planet, requestApiPlanet } }>
+    <PlanetContext.Provider value={ contextValue }>
       {children}
     </PlanetContext.Provider>
   );
