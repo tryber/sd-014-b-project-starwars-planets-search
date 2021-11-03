898d87e3a55cc48ed775a0980b32a50d9252447b
Danilo <danilo.santossouza@outlook.com>
2021-11-03 20:38:29 -0300

    Requisito 2 resolvido

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,10 +3,12 @@ import PlanetApiContext from '../context/PlanetContext';
 import HeaderTable from './HeaderTable';
 import Header from './Header';
 import TableBody from './TableBody';
+// import Filter from './Filter';
 
 function Table() {
   const { dataPlanet } = useContext(PlanetApiContext);
   const [isLoading, setIsLoading] = useState(false);
+  const [values, setValues] = useState('');
 
   useEffect(() => {
     if (dataPlanet.length > 0) {
@@ -14,15 +16,39 @@ function Table() {
     }
   }, [dataPlanet]);
 
+  console.log(values);
+
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setValues({ ...values, filters: { [name]: value } });
+  }
+
+  console.log(isLoading);
+
   return (
     <div>
       <Header />
+      <div>
+        <label htmlFor="filterName">
+          <input
+            data-testid="name-filter"
+            id="filterName"
+            name="filterByName"
+            type="text"
+            onChange={ handleChange }
+            placeholder="Filtrar por nome"
+          />
+        </label>
+      </div>
       <table>
         <thead>
           <HeaderTable />
         </thead>
         <tbody>
-          {isLoading && dataPlanet
+          {!values && dataPlanet
+            .map((planet, index) => <TableBody key={ index } planet={ planet } />)}
+          {values && dataPlanet
+            .filter((planet) => planet.name.includes(values.filters.filterByName))
             .map((planet, index) => <TableBody key={ index } planet={ planet } />)}
         </tbody>
       </table>
