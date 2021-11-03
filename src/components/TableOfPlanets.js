04c6d541657f9d1ba5e5c6a555fbede3c13ed9d8
Juliana Braga <juliana4braga@gmail.com>
2021-11-03 10:43:39 -0300

    Refatora tabela

--- src/components/TableOfPlanets.js
+++ src/components/TableOfPlanets.js
@@ -2,6 +2,10 @@ import React, { useContext } from 'react';
 import planetsContext from '../context/PlanetsContext';
 import '../styles/Table.css';
 
+const itemsHeader = ['Name', 'Rotation Period', 'Orbital Period',
+  'Diameter', 'Climate', 'Gravity', 'Terrain', 'Surface Water',
+  'Population', 'Films', 'Created', 'Edited', 'URL'];
+
 function TableOfPlanets() {
   const { planets, filterByName } = useContext(planetsContext);
   if (filterByName.name !== '') {
@@ -10,20 +14,13 @@ function TableOfPlanets() {
         .includes(filterByName.name.toLowerCase()));
     return (
       <table>
-        <tr>.</tr>
-        <th>Name</th>
-        <th>Rotation period</th>
-        <th>Orbital period</th>
-        <th>Diameter</th>
-        <th>Climate</th>
-        <th>Gravity</th>
-        <th>Terrain</th>
-        <th>Surface Water</th>
-        <th>Population</th>
-        <th>Films</th>
-        <th>Created</th>
-        <th>Edited</th>
-        <th>Url</th>
+        <thead>
+          <tr>
+            { itemsHeader.map((item, index) => (
+              <th key={ index }>{ item }</th>
+            )) }
+          </tr>
+        </thead>
         { planetsFiltered.map((planet, index) => (
           <tr key={ index }>
             <td className="column" key={ planet.name }>{ planet.name }</td>
@@ -71,62 +68,32 @@ function TableOfPlanets() {
   }
   return (
     <table>
-      <tr>.</tr>
-      <th>Name</th>
-      <th>Rotation period</th>
-      <th>Orbital period</th>
-      <th>Diameter</th>
-      <th>Climate</th>
-      <th>Gravity</th>
-      <th>Terrain</th>
-      <th>Surface Water</th>
-      <th>Population</th>
-      <th>Films</th>
-      <th>Created</th>
-      <th>Edited</th>
-      <th>Url</th>
-      { planets.map((planet, index) => (
-        <tr key={ index }>
-          <td className="column" key={ planet.name }>{ planet.name }</td>
-          <td
-            className="column"
-            key={ planet.rotation_period }
-          >
-            { planet.rotation_period }
-          </td>
-          <td
-            className="column"
-            key={ planet.orbital_period }
-          >
-            { planet.orbital_period }
-          </td>
-          <td
-            className="column"
-            key={ planet.diameter }
-          >
-            { planet.diameter }
-          </td>
-          <td className="column" key={ planet.climate }>{ planet.climate }</td>
-          <td className="column" key={ planet.gravity }>{ planet.gravity }</td>
-          <td className="column" key={ planet.terrain }>{ planet.terrain }</td>
-          <td
-            className="column"
-            key={ planet.surface_water }
-          >
-            { planet.surface_water }
-          </td>
-          <td
-            className="column"
-            key={ planet.population }
-          >
-            { planet.population }
-          </td>
-          <td className="column" key={ planet.films }>{ planet.films }</td>
-          <td className="column" key={ planet.created }>{ planet.created }</td>
-          <td className="column" key={ planet.edited }>{ planet.edited }</td>
-          <td className="column" key={ planet.url }>{ planet.url }</td>
+      <thead>
+        <tr>
+          {itemsHeader.map((item, index) => (
+            <th key={ index }>{item}</th>
+          ))}
         </tr>
-      )) }
+      </thead>
+      <tbody>
+        {planets.map((planet, index) => (
+          <tr key={ index }>
+            <td key={ planet.name }>{ planet.name }</td>
+            <td key={ planet.name }>{ planet.rotation_period }</td>
+            <td key={ planet.name }>{ planet.orbital_period }</td>
+            <td key={ planet.name }>{ planet.diameter }</td>
+            <td key={ planet.name }>{ planet.climate }</td>
+            <td key={ planet.name }>{ planet.gravity }</td>
+            <td key={ planet.name }>{ planet.terrain }</td>
+            <td key={ planet.name }>{ planet.surface_water }</td>
+            <td key={ planet.name }>{ planet.population }</td>
+            <td key={ planet.name }>{ planet.films }</td>
+            <td key={ planet.name }>{ planet.created }</td>
+            <td key={ planet.name }>{ planet.edited }</td>
+            <td key={ planet.name }>{ planet.url }</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
