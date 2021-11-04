e59e9dc153ccbfd86da1f3f524dab43fbe92325f
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-11-04 18:50:43 -0300

    Question 1 resolved

---
+++ src/components/Table.jsx
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+
+function Table() {
+  const { data, loading } = useContext(StarWarsContext);
+  return (
+    <section>
+      {!loading
+        ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Rotation Period</th>
+                <th>Orbital Period</th>
+                <th>Diameter</th>
+                <th>Climate</th>
+                <th>Gravity</th>
+                <th>Terrain</th>
+                <th>Surface Water</th>
+                <th>Population</th>
+                <th>Films</th>
+                <th>Created</th>
+                <th>Edited</th>
+                <th>URL</th>
+              </tr>
+            </thead>
+            <tbody>
+              { data.map((eachPlanet) => (
+                <tr key={ eachPlanet.name }>
+                  <td>{ eachPlanet.name }</td>
+                  <td>{ eachPlanet.orbital_period }</td>
+                  <td>{ eachPlanet.diameter }</td>
+                  <td>{ eachPlanet.climate }</td>
+                  <td>{ eachPlanet.gravity }</td>
+                  <td>{ eachPlanet.terrain }</td>
+                  <td>{ eachPlanet.surface_water }</td>
+                  <td>{ eachPlanet.population }</td>
+                  <td>{ eachPlanet.residents }</td>
+                  <td>{ eachPlanet.films }</td>
+                  <td>{ eachPlanet.created }</td>
+                  <td>{ eachPlanet.edited }</td>
+                  <td>{ eachPlanet.url }</td>
+                </tr>
+              )) }
+            </tbody>
+          </table>) : (<span>Carregando...</span>)}
+    </section>
+  );
+}
+
+export default Table;
---
+++ src/context/StarWarsProvider.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import StarWarsContext from './StarWarsContext';
+
+function StarWarsProvider({ children }) {
+  const [data, setData] = useState([]);
+  // Dica do Matheus Gonzaga - 14B, decidi colocar também no meu
+  const [loading, setLoading] = useState([]);
+
+  // Fetch data from API
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((response) => response.json())
+      .then(({ results }) => {
+        setData(results);
+        setLoading(false);
+      });
+  }, []);
+
+  const starWarsContext = {
+    data,
+    loading,
+  };
+
+  return (
+    <main>
+      <StarWarsContext.Provider value={ starWarsContext }>
+        { children }
+      </StarWarsContext.Provider>
+    </main>
+  );
+}
+
+StarWarsProvider.propTypes = {
+  children: PropTypes.shape(PropTypes.func).isRequired,
+};
+
+export default StarWarsProvider;
