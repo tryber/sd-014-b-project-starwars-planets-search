58e015ab57c7d9b23f45cea467c9e32ba48bc611
lucas.alves <lucas.alves@mtechsp.com.br>
2021-11-03 23:17:52 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Context from './context/context';
+import Table from './components/Table';
 
 function App() {
+  const [data, setData] = useState([]);
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+  const api = async () => {
+    const response = await fetch(url);
+    const obj = await response.json();
+    const result = obj.results;
+    setData(result);
+    return result;
+  };
+
+  const contextValue = {
+    data,
+    setData,
+  };
+  // ajuda do Diogo Fiuza
+  // consultei também o site https://pt-br.reactjs.org/docs/hooks-reference.html
+  useEffect(() => {
+    api();
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    <Context.Provider value={ contextValue }>
+      <Table />
+    </Context.Provider>
   );
 }
 
