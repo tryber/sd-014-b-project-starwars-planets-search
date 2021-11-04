c6ec724adfcd8bb72f898ce77dc8c9e04b9df7ab
LucasAccurcio <lucas.accurcio@gmail.com>
2021-11-04 00:26:35 -0300

    Avaliador nao passando

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import './App.css';
 import FilterByName from './components/FilterByName';
 import FilterByNumber from './components/FilterByNumber';
-import FiltersSelected from './components/FiltersSelected';
 import Table from './components/Table';
 import Context from './context/Context';
 import Provider from './context/Provider';
@@ -16,7 +15,6 @@ function App() {
         <h1>Projeto Star Wars Trybe</h1>
         <FilterByName />
         <FilterByNumber />
-        <FiltersSelected />
         { isLoading ? <h1>Loading...</h1> : <Table /> }
       </div>
     </Provider>
