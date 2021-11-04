5309e325f65b30cd77fb96f612fd4ed7d69d899a
enzo <enzo@pop-os.localdomain>
2021-11-04 20:35:55 -0300

    Requisito 4

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,15 +1,17 @@
 import React, { useContext, useState } from 'react';
 import DataContext from '../context/Context';
 
-const options = ['population', 'orbital_period',
+const columnOptionsArray = ['population', 'orbital_period',
   'diameter', 'rotation_period', 'surface_water'];
+
+const comparisonOptions = ['maior que', 'igual a', 'menor que'];
 
 const DropDown = () => {
   const { filterObject, setFilter, planets, setPlanets } = useContext(DataContext);
   const [column, setColumn] = useState('population');
   const [comparison, setComparison] = useState('maior que');
   const [numberValue, setNumberValue] = useState('100000');
-
+  const [columnOptions, setColumnOptions] = useState(columnOptionsArray);
   // lógica da função onCLick retirada do repo do Michael Caxias
   // ref: https://github.com/tryber/sd-014-b-project-starwars-planets-search/pull/1/commits/1e67a41f29c6db5b76634fc6d6cdcfe337aa6e07
   const onClick = () => {
@@ -22,6 +24,9 @@ const DropDown = () => {
         ],
       },
     });
+
+    const filteredColumnOptions = columnOptions.filter((option) => option !== column);
+    setColumnOptions(filteredColumnOptions);
 
     const filterByNumeric = planets.filter((planet) => {
       switch (comparison) {
@@ -38,6 +43,10 @@ const DropDown = () => {
     setPlanets(filterByNumeric);
   };
 
+  const mapOptions = (array) => array.map(
+    (option) => <option key={ option }>{option}</option>,
+  );
+
   return (
     <>
       <select
@@ -46,20 +55,14 @@ const DropDown = () => {
         onChange={ ({ target: { value } }) => setColumn(value) }
         name="column"
       >
-        { options.map((option) => (
-          <option key={ option }>
-            {option}
-          </option>
-        ))}
+        {mapOptions(columnOptions)}
       </select>
       <select
         data-testid="comparison-filter"
         value={ comparison }
         onChange={ ({ target: { value } }) => setComparison(value) }
       >
-        <option>maior que</option>
-        <option>igual a</option>
-        <option>menor que</option>
+        {mapOptions(comparisonOptions)}
       </select>
       <input
         type="number"
