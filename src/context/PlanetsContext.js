c4d991d451240d4b0b231bcac787806a7c70a1dc
icaroemanuel <icaro.emanuel@outlook.com>
2021-11-05 00:12:50 -0300

    Salvando os dados da api no contexto

--- src/context/PlanetsContext.js
+++ src/context/PlanetsContext.js
@@ -2,4 +2,4 @@ import { createContext } from 'react';
 
 const PlanetsContext = createContext();
 
-export default planetsContext;
+export default PlanetsContext;
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PlanetsContext from './PlanetsContext';
+import fetchPlanets from '../services/fetchPlanetsApi';
 
 function PlanetsProvider({ children }) {
+  const [ data, setData ] = useState([]);
+  const [user, setUser] = useState('Icaro');
+
+  const planetsData = async() => {
+    const planets = await fetchPlanets();
+    setData(planets);
+  };
+  
+  useEffect(() => {
+    planetsData();
+  }, []);
+  
+  const contextValue = {
+    data,
+    planetsData,
+    user,
+  }
+  
+  // const returnPlanets = data.map((planet) => planet.name)
+  // console.log(returnPlanets);
+
   return(
-    <PlanetsContext.Provider>
+    <PlanetsContext.Provider value={contextValue}>
       {children}
     </PlanetsContext.Provider>
   );
