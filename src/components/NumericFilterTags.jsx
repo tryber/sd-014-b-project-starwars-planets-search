33e9d35422aff1236b8ba0397090df2b387364f8
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2021-11-05 10:28:28 -0300

    Requisito 5 OK

--- src/components/NumericFilterTags.jsx
+++ src/components/NumericFilterTags.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { useFilters } from '../context/Filters';
 
 const NumericFilterTags = () => {
-  const { filters } = useFilters();
+  const { filters, setFilters } = useFilters();
 
   const { filterByNumericValues } = filters;
+
+  const deleteFilter = (tagObject) => {
+    const newFilters = filterByNumericValues.filter((object) => object !== tagObject);
+    setFilters({ ...filters, filterByNumericValues: newFilters });
+  };
 
   const createTags = () => {
     let output = '';
     if (filterByNumericValues.length > 0) {
       output = filterByNumericValues.map((object, index) => (
-        <div key={ index }>
+        <div key={ index } data-testid="filter">
           <p>{ `${object.column} é ${object.comparison} ${object.value}` }</p>
-          <button type="button">X</button>
+          <button type="button" onClick={ () => deleteFilter(object) }>X</button>
         </div>
       ));
     }
