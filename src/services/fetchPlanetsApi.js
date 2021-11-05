1dbb33f9aa26fb78ac9cdccca92f1614611ee03b
icaroemanuel <icaro.emanuel@outlook.com>
2021-11-05 00:11:30 -0300

    Requisição dos dados da api

---
+++ src/services/fetchPlanetsApi.js
@@ -0,0 +1,14 @@
+const fetchPlanets = async() => {
+  // const URL_PLANETS = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  // Atingi o limite de acesso da API e o Vitor Veloso sugeriu a URL abaixo
+  const URL = 'https://swapi.dev/api/planets/'
+  try {
+    const response = await fetch(URL);
+    const resolve = await response.json();
+    return resolve.results;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+export default fetchPlanets;
