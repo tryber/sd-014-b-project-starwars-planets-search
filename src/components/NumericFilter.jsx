cf96ded1bc43bde19dbcd41f9e0956a796132e59
Bruno Trindade <brunotrndd@gmail.com>
2021-11-04 22:14:02 -0300

    requisito 4

--- src/components/NumericFilter.jsx
+++ src/components/NumericFilter.jsx
@@ -2,75 +2,66 @@ import React, { useContext, useState } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 const HUNDRED_THOUSAND = 100000;
+const PARAMETERS = [
+  'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+];
 
 function NumericFilter() {
   const [inputParameter, setInputParameter] = useState('population');
   const [inputComparison, setInputComparison] = useState('maior que');
   const [inputNumber, setInputNumber] = useState(HUNDRED_THOUSAND);
+  const [pars, setPars] = useState(PARAMETERS);
   const { filters, setFilters, setData, backup } = useContext(PlanetsContext);
 
   function handleParameterChange(event) {
     const { value } = event.target;
     setInputParameter(value);
-    setFilters({
-      ...filters,
-      filterByNumericValues: [
-        {
-          ...filters.filterByNumericValues[0],
-          column: value,
-        },
-      ],
-    });
   }
 
   function handleComparisonChange(event) {
     const { value } = event.target;
     setInputComparison(value);
-    setFilters({
-      ...filters,
-      filterByNumericValues: [
-        {
-          ...filters.filterByNumericValues[0],
-          comparison: value,
-        },
-      ],
-    });
   }
 
   function handleNumberChange(event) {
     const { value } = event.target;
     setInputNumber(value);
-    setFilters({
-      ...filters,
-      filterByNumericValues: [
-        {
-          ...filters.filterByNumericValues[0],
-          value,
-        },
-      ],
-    });
   }
 
   function handleClick() {
     let filtered;
+    setFilters({
+      ...filters,
+      filterByNumericValues: [
+        ...filters.filterByNumericValues,
+        {
+          column: inputParameter,
+          comparison: inputComparison,
+          value: inputNumber,
+        },
+      ],
+    });
     if (inputComparison === 'maior que') {
       filtered = backup.filter(
-        (planet) => (+planet[inputParameter] > +inputNumber
-          || planet[inputParameter] === 'unknown'),
+        (planet) => (+planet[inputParameter] > +inputNumber),
       );
-      console.log(filtered);
       setData(filtered);
+      const filteredPars = PARAMETERS.filter((par) => par !== inputParameter);
+      setPars(filteredPars);
     } else if (inputComparison === 'menor que') {
       filtered = backup.filter(
         (planet) => (+planet[inputParameter] < +inputNumber),
       );
-      console.log(filtered);
       setData(filtered);
+      const filteredPars = PARAMETERS.filter((par) => par !== inputParameter);
+      setPars(filteredPars);
     } else if (inputComparison === 'igual a') {
       filtered = backup.filter(
         (planet) => (planet[inputParameter] === inputNumber),
       );
       setData(filtered);
+      const filteredPars = PARAMETERS.filter((par) => par !== inputParameter);
+      setPars(filteredPars);
     }
   }
 
@@ -81,11 +72,7 @@ function NumericFilter() {
         onChange={ handleParameterChange }
         value={ inputParameter }
       >
-        <option>population</option>
-        <option>orbital_period</option>
-        <option>diameter</option>
-        <option>rotation_period</option>
-        <option>surface_water</option>
+        { pars.map((par, index) => <option key={ index }>{ par }</option>) }
       </select>
       <select
         data-testid="comparison-filter"
