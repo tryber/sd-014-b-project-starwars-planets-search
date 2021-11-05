f967aefa56bfbd8fd51f9e131e81e6c3eefec594
Thales Lima <thales.bplima@gmail.com>
2021-11-05 00:52:44 -0300

    impl feats on form and provider

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -2,16 +2,97 @@ import React, { useContext } from 'react';
 import PlanetContext from '../Context/PlanetContext';
 
 function SearchForm() {
-  const { name, setNames } = useContext(PlanetContext);
+  const {
+    name,
+    setNames,
+    column,
+    setColumn,
+    filteredColumnOptions,
+    comparision,
+    setComparision,
+    value,
+    setValue,
+    filters: { filterByNumericValues },
+    setNumericValues,
+  } = useContext(PlanetContext);
+
+  const handleClick = (filterColumn) => { // testar depois do 4
+    const keepFilter = filterByNumericValues
+      .filter((target) => target.column !== filterColumn);
+    setNumericValues(keepFilter);
+  };
+
   return (
-    <label htmlFor="name-filter">
-      <input
-        data-testid="name-filter"
-        type="text"
-        value={ name }
-        onChange={ (event) => setNames(event.target.value) }
-      />
-    </label>
+    <form>
+      <label htmlFor="name-filter">
+        Nome:
+        <input
+          data-testid="name-filter"
+          type="text"
+          value={ name }
+          onChange={ (event) => setNames(event.target.value) }
+        />
+      </label>
+      <label htmlFor="column-filter">
+        Característica:
+        <select
+          data-testid="column-filter"
+          value={ column }
+          onChange={ (event) => setColumn(event.target.value) }
+        >
+          { filteredColumnOptions.map((option, index) => (
+            <option value={ option } key={ index }>
+              {option}
+            </option>
+          )) }
+        </select>
+      </label>
+      <label htmlFor="comparison-filter">
+        Condição:
+        <select
+          data-testid="comparison-filter"
+          value={ comparision }
+          onChange={ (event) => setComparision(event.target.value) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+      </label>
+      <label htmlFor="value-filter">
+        Quantidade:
+        <input
+          data-testid="value-filter"
+          type="number"
+          value={ value }
+          onChange={ (event) => setValue(event.target.value) }
+        />
+      </label>
+      <button
+        data-testid="button-filter"
+        type="button"
+        onClick={ () => setNumericValues([
+          ...filterByNumericValues,
+          { column, comparision, value },
+        ]) }
+      >
+        Adicionar Filtro
+      </button>
+      <div>
+        {filterByNumericValues.map((filter, index) => (
+          <span key={ index }>
+            {`Mostrando filtros para:
+            ${filter.column} ${filter.comparision} ${filter.value}`}
+            <button
+              type="button"
+              onClick={ () => handleClick(filter.column) }
+            >
+              X
+            </button>
+          </span>
+        ))}
+      </div>
+    </form>
   );
 }
 
--- src/Provider/PlanetProvider.jsx
+++ src/Provider/PlanetProvider.jsx
@@ -1,20 +1,33 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import PlanetContext from '../Context/PlanetContext';
 import fetchPlanets from '../services/fetchPlanets';
+import PlanetContext from '../Context/PlanetContext';
 
 export default function PlanetProvider({ children }) {
+  const allColumnOptions = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
   const [planets, setPlanets] = useState([]);
   const [name, setNames] = useState('');
   const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [column, setColumn] = useState('population');
+  const [comparision, setComparision] = useState('maior que');
+  const [value, setValue] = useState(0);
+  const [filterByNumericValues, setNumericValues] = useState([]);
+  const [filteredColumnOptions, setOptions] = useState(allColumnOptions);
 
   useEffect(() => {
     const getPlanets = async () => {
       const response = await fetchPlanets();
       setPlanets(response.results);
       setFilteredPlanets(response.results);
-    }; getPlanets();
+    };
+    getPlanets();
   }, []);
 
   useEffect(() => {
@@ -24,27 +37,39 @@ export default function PlanetProvider({ children }) {
     setFilteredPlanets(planetsWithFilter);
   }, [name]);
 
+  useEffect(() => {
+    const columns = filterByNumericValues.map((filter) => filter.column);
+    const filtersToBeKept = allColumnOptions
+      .filter((options) => !columns.includes(options)); // arrays cujos quais não têm a coluna já filtrada
+    setOptions(filtersToBeKept);
+  }, [filterByNumericValues]);
+
+  useEffect(() => {
+    setColumn(filteredColumnOptions[0]);
+  }, [filteredColumnOptions]);
+
   const planetState = {
     setNames,
+    setColumn,
+    setComparision,
+    setValue,
+    setNumericValues,
+    column,
+    filteredColumnOptions,
+    comparision,
+    value,
     filteredPlanets,
-    filters:
-    {
+    filters: {
       filterByName: {
         name,
       },
-      filterByNumericValues: [
-        {
-          column: '',
-          comparison: '',
-          value: '',
-        },
-      ],
+      filterByNumericValues,
     },
   };
 
   return (
     <PlanetContext.Provider value={ planetState }>
-      { children }
+      {children}
     </PlanetContext.Provider>
   );
 }
