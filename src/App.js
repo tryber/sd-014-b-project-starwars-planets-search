6161eab8fc9f233ce7eede8060892871f1fdf29d
gbezzuoli <guilhermebezzuoli@outlook.com>
2021-11-07 15:19:43 -0300

    lint fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import GlobalStorage from './context/GlobalStorage';
 import Table from './components/Table';
-import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div>
       <GlobalStorage>
-        <SearchBar />
         <Table />
       </GlobalStorage>
     </div>
