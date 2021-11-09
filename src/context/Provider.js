5245e37f244b28da79800400fc3cafc2ff52bf52
Anderson Leite <anderson.leite.bsi@gmail.com>
2021-11-09 17:08:13 -0300

    update Provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -2,21 +2,51 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import StarwarsContext from './Context';
 
+const INITIAL_FILTERS = {
+  filters: {
+    filterByName: {
+      name: '',
+    },
+  },
+};
+
 const StarwarsProvider = ({ children }) => {
   const [data, setData] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [filters, setFilters] = useState(INITIAL_FILTERS);
 
   const fetchPlanets = async () => {
     const url = 'https://swapi-trybe.herokuapp.com/api/planets';
     const request = await fetch(url);
     const resolve = await request.json();
     setData(resolve.results);
+    setPlanets(resolve.results);
+  };
+
+  const filterPlanetByName = () => {
+    const { filterByName } = filters.filters;
+    const { name } = filterByName;
+    // ref: https://www.w3schools.com/jsref/jsref_filter.asp
+    const filteredPlanets = data.filter(
+      // ref: https://www.w3schools.com/jsref/jsref_includes.asp
+      (planet) => planet.name.toLowerCase().includes(name.toLowerCase()),
+    );
+    return setPlanets(filteredPlanets);
   };
 
   useEffect(() => {
     fetchPlanets();
   }, []);
 
-  const state = { data };
+  useEffect(() => {
+    filterPlanetByName();
+  }, [filters]);
+
+  const state = {
+    planets,
+    filters,
+    setFilters,
+  };
 
   return (
     <StarwarsContext.Provider value={ state }>
