8afcc1795e9dc676e4f3909f273cc20fea87dbcf
Anderson Leite <anderson.leite.bsi@gmail.com>
2021-11-09 17:13:12 -0300

    req: 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import StarwarsProvider from './context/Provider';
+import InputFindPlanets from './components/InputFindPlanets';
 import Table from './components/Table';
 import './App.css';
 
 function App() {
   return (
     <StarwarsProvider>
+      <InputFindPlanets />
       <Table />
     </StarwarsProvider>
   );
