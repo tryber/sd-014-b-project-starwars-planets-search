0a61e43f6b8c013f45737c1add1cf674c8f435e3
brenoMatias <matiasbreno19@gmail.com>
2021-11-10 11:54:40 -0300

    fix reqs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+
+  
 import React from 'react';
 import './App.css';
-import Table from './components/Table';
+import NumericFilter from './components/NumericFilter';
 import SearchBar from './components/SearchBar';
+import Table from './components/Table';
 import PlanetProvider from './context/PlanetProvider';
 
 function App() {
   return (
     <PlanetProvider>
       <main>
+        <h1>StarWars Planets Search</h1>
         <SearchBar />
+        <NumericFilter />
         <Table />
       </main>
     </PlanetProvider>
