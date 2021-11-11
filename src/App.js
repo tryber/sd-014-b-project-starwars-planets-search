e75541761b193ae7ac7aa3ac9de41f5fcf10fabd
Daniel-Coelho <daniel.acoelho@outlook.com>
2021-11-11 07:59:36 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import TablePlanets from './components/TablePlanets';
 import Provider from './context/Provider';
+import PlanetsSearch from './components/PlanetsSearch';
 
 function App() {
   return (
     <Provider>
+      <PlanetsSearch />
       <TablePlanets />
     </Provider>
   );
--- src/components/TablePlanets.jsx
+++ src/components/TablePlanets.jsx
@@ -18,33 +18,37 @@ const planets = [
 ];
 
 function TablePlanets() {
-  const { response } = useContext(Context);
+  const { response, nameFilter: { filters } } = useContext(Context);
+  const { filterByName: { name } } = filters;
   return (
     <div>
       <table>
         <thead>
           <tr>
-            { planets.map((name) => <th key={ name }>{name}</th>) }
+            { planets.map((index) => <th key={ index }>{index}</th>) }
           </tr>
         </thead>
         <tbody>
-          { response.map((index) => (
-            <tr key={ index.url }>
-              <td>{ index.name }</td>
-              <td>{ index.rotation_period }</td>
-              <td>{ index.orbital_period }</td>
-              <td>{ index.diameter }</td>
-              <td>{ index.climate }</td>
-              <td>{ index.gravity }</td>
-              <td>{ index.terrain }</td>
-              <td>{ index.surface_water }</td>
-              <td>{ index.population }</td>
-              <td>{ index.residents.length }</td>
-              <td>{ index.films.length }</td>
-              <td>{ index.created }</td>
-              <td>{ index.edited }</td>
-            </tr>
-          )) }
+          { response.filter((
+            value,
+          ) => value.name.toLowerCase().includes(name.toLowerCase()))
+            .map((index, planet) => (
+              <tr key={ planet }>
+                <td>{ index.name }</td>
+                <td>{ index.rotation_period }</td>
+                <td>{ index.orbital_period }</td>
+                <td>{ index.diameter }</td>
+                <td>{ index.climate }</td>
+                <td>{ index.gravity }</td>
+                <td>{ index.terrain }</td>
+                <td>{ index.surface_water }</td>
+                <td>{ index.population }</td>
+                <td>{ index.residents.length }</td>
+                <td>{ index.films.length }</td>
+                <td>{ index.created }</td>
+                <td>{ index.edited }</td>
+              </tr>
+            )) }
         </tbody>
       </table>
     </div>
