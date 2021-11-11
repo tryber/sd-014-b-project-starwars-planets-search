e0ea6a41acbcd55ff79db00ba7e38d1c9b7eb01a
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-11-11 11:17:01 -0300

    Fix lint issues

--- src/components/SearchArea.jsx
+++ src/components/SearchArea.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import ColumnFilter from './ColumnFilter';
 import StarWarsContext from '../context/StarWarsContext';
 
