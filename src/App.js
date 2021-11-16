b33df09e64e5e4ce3627afb9840d33e7ef4e9a43
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-11-15 22:34:15 -0300

    requisitos 3 e 4 concluidos, ainda com bug no 2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Table from './components/Table';
 import { PlanetsProvider } from './context/Planets';
 import FilterByName from './components/FilterByName';
+import FilterByNumber from './components/FilterByNumber';
 
 function App() {
   return (
     <PlanetsProvider>
       <FilterByName />
+      <FilterByNumber />
       <Table />
     </PlanetsProvider>
   );
--- src/context/Planets.js
+++ src/context/Planets.js
@@ -9,6 +9,11 @@ const INITIAL_FILTERS = {
   filterByName: {
     name: '',
   },
+  filterByNumericValues: [],
+  order: {
+    column: 'name',
+    sort: 'ASC',
+  },
 };
 
 const PlanetsContext = createContext(INITIAL_STATE);
@@ -34,12 +39,62 @@ export function PlanetsProvider({ children }) {
     filterPlanetsByName(value);
   };
 
+  const filterPlanetsByValues = (valuesFilters, planetsData) => {
+    if (valuesFilters.length !== 0) {
+      valuesFilters.forEach((filter) => {
+        const filtered = planetsData.filter((item) => {
+          const columnValue = Number(item[filter.column]);
+          const filterValue = Number(filter.value);
+          if (filter.comparison === 'maior que') {
+            return columnValue > filterValue;
+          }
+          if (filter.comparison === 'menor que') {
+            return columnValue < filterValue;
+          }
+          return columnValue === filterValue;
+        });
+        setPlanets(filtered);
+      });
+    }
+  };
+
+  const handleFilterValues = (filterValue) => {
+    const updateFilterValues = filters.filterByNumericValues.concat(filterValue);
+    setFilters({
+      ...filters,
+      filterByNumericValues: updateFilterValues,
+    });
+    filterPlanetsByValues(updateFilterValues, planets);
+  };
+
+  const resetPlanets = () => {
+    setPlanets(planets);
+  };
+
+  const deleteNumericFilter = (column) => {
+    const updateNumericFilter = filters.filterByNumericValues.filter(
+      (item) => item.column !== column,
+    );
+    if (updateNumericFilter.length > 0) {
+      filterPlanetsByValues(updateNumericFilter, planets);
+    } else {
+      resetPlanets();
+    }
+    setFilters({
+      ...filters,
+      filterByNumericValues: updateNumericFilter,
+    });
+  };
+
   const context = {
     planets,
     setPlanets,
     filters,
     setFilters,
     handleFilterByName,
+    handleFilterValues,
+    filterByNumericValues: filters.filterByNumericValues,
+    deleteNumericFilter,
   };
 
   return (
