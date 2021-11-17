a62e17f4978097ddbc00e422acc1461b74ebffef
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-11-17 15:48:48 -0300

    Requisito 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FetchProvider from '../context/fetchProvider';
 import Table from '../components/Table';
+import Inputs from '../components/Inputs';
 
 function Home() {
   return (
     <div>
       <FetchProvider>
+        <Inputs />
         <Table />
       </FetchProvider>
     </div>
