226e1f41ad64d38a6049927ae7799f2983071407
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-26 00:57:12 -0400

    Apaga os comentários do MyProvider

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -4,19 +4,13 @@ import MyContext from './MyContext';
 import { getPlanets } from '../services';
 
 export default function MyProvider({ children }) {
-  // aqui colocamos toda a lógica necessária para obtenção dos dados que vamos prover. Ex: requisições a APIs (fetch), Gerenciamento de estado (useState), etc
-
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  // usar o userEfets e fazer a lógica async da requisição
   useEffect(() => {
     (async () => {
-      // setar isLoading para true
       setIsLoading(true);
-      // salvar os dados no estado
       setData(await getPlanets());
-      // setar o isLoading para false
       setIsLoading(true);
     })();
   }, []);
