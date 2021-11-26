3938ac9fc08b2d2b8818f2c43db628a181c36de5
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-26 02:36:33 -0400

    altera o caminho do comp Table no App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Table from './components/Table';
+import Table from './components/table/Table';
 import MyProvider from './context/MyProvider';
 
 function App() {
