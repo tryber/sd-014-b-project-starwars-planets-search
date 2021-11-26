51bd3a65580ccdff13322bbe34fce1448f65860e
GustavoPD <gudalmoro@gmail.com>
2021-11-26 04:54:08 -0300

    Provider e context p requisiçao de planetas

---
+++ src/components/Table.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+function Table() {
+  return (
+    <div>
+      <p>Table</p>
+    </div>
+  );
+}
+
+export default Table;
---
+++ src/context/AppProvider.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import AppContext from './AppContext';
+
+function AppProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    const URL = 'https://swapi-trybe.herokuapp.com/api/planets';
+    const response = await fetch(URL);
+    const listPlanets = await response.json();
+    setData(listPlanets.results);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const stateDefault = {
+    data,
+  };
+
+  return (
+    <AppContext.Provider value={ stateDefault }>
+      { children }
+    </AppContext.Provider>
+  );
+}
+
+AppProvider.propTypes = {
+  children: PropTypes.node,
+}.isRequired;
+
+export default AppProvider;
