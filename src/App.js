9508a8bb373bae19cee86ca5bca8dcd573180e15
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-25 21:38:08 -0400

    Cria o Mycontext.js, cria o Table.jsx e remove console.log

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getPlanets } from './services';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
   const [planets, setPlanets] = useState([]);
@@ -9,14 +10,13 @@ function App() {
     (async () => setPlanets(await getPlanets()))();
   }, []);
 
-  console.log(planets);
-
   return (
     <>
       <span>Hello, App!</span>
       <ul>
         {planets.map((p) => <li key={ p.name }>{p.name}</li>)}
       </ul>
+      <Table />
     </>
   );
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Table() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Planets
+          </th>
+        </tr>
+      </thead>
+    </table>
+  );
+}
