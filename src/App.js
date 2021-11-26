acdc0d28cf42c9a979546d6c3fa5ddc2ffa151a1
GustavoPD <gudalmoro@gmail.com>
2021-11-26 05:35:59 -0300

    Tabela de planetas - requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Table from './components/Table';
+import AppProvider from './context/AppProvider';
 
 function App() {
   return (
-    <span>
+    <AppProvider>
       <Table />
-    </span>
+    </AppProvider>
   );
 }
 
