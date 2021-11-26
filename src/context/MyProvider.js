a3d6b05ebb4d61202d766881c78bb6c6e7e7661e
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-26 00:53:33 -0400

    Criei o estado global no provider

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './MyContext';
+import { getPlanets } from '../services';
 
 export default function MyProvider({ children }) {
-  const contextValue = {};
+  // aqui colocamos toda a lógica necessária para obtenção dos dados que vamos prover. Ex: requisições a APIs (fetch), Gerenciamento de estado (useState), etc
+
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // usar o userEfets e fazer a lógica async da requisição
+  useEffect(() => {
+    (async () => {
+      // setar isLoading para true
+      setIsLoading(true);
+      // salvar os dados no estado
+      setData(await getPlanets());
+      // setar o isLoading para false
+      setIsLoading(true);
+    })();
+  }, []);
 
   return (
-    <MyContext.Provider value={ contextValue }>
+    <MyContext.Provider value={ { data, isLoading } }>
       {children}
     </MyContext.Provider>
   );
