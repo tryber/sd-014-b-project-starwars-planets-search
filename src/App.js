8c7461888ac84e6d92c8766349bdc0cb20b52038
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-28 16:08:37 -0400

    cria o componente filter e seu arquivo css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Filter from './components/filter/Filter';
 import Table from './components/table/Table';
 import MyProvider from './context/MyProvider';
 
 function App() {
   return (
     <MyProvider>
+      <Filter />
       <Table />
     </MyProvider>
   );
---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './filter.css';
+
+export default function Filter() {
+  return (
+    <div>
+      Eu sou o Filter
+    </div>
+  );
+}
