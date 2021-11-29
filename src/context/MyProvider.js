5859b502be343bb773228e0db7666910d602e1fa
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-29 07:20:23 -0400

    Cria um novo estado no provider, refatora proptypes

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -5,18 +5,29 @@ import { getPlanets } from '../services';
 
 export default function MyProvider({ children }) {
   const [data, setData] = useState([]);
+  const [filters, setFilters] = useState({
+    filterByName: {
+      name: '',
+    },
+  });
 
   useEffect(() => {
     (async () => setData(await getPlanets()))();
   }, []);
 
+  const contextValue = {
+    data,
+    filters,
+    setFilters,
+  };
+
   return (
-    <MyContext.Provider value={ data }>
+    <MyContext.Provider value={ contextValue }>
       {children}
     </MyContext.Provider>
   );
 }
 
 MyProvider.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.node.isRequired,
 };
