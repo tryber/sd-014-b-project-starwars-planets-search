7f5ca4c5ebb4d4e5dca039c6e3e61f7cd698639b
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-11-30 12:57:46 -0400

    6

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -16,7 +16,7 @@ export default function Table() {
       <tbody>
         {filteredPlanets.map((planet, index) => (
           <tr key={ index }>
-            <td>{planet.name}</td>
+            <td data-testid="planet-name">{planet.name}</td>
             <td>{planet.rotation_period}</td>
             <td>{planet.orbital_period}</td>
             <td>{planet.diameter}</td>
