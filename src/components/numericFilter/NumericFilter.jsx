f7ae9f8baa906f784fb1ee2e1a17a80b4ddd5628
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-12-02 09:47:34 -0400

    Colocar um alerta se o filtro estiver vazio

--- src/components/numericFilter/NumericFilter.jsx
+++ src/components/numericFilter/NumericFilter.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import MyContext from '../../context/MyContext';
+import OrderFilter from '../orderFilter/OrderFilter';
 
 export default function NumericFilter() {
   const { filteredColumns, filterInputs, setInputs,
@@ -11,7 +12,7 @@ export default function NumericFilter() {
     });
   };
 
-  const alert = (test) => (test
+  const alert = (input) => (!filterInputs.column
     ? <p style={ { color: 'red' } }>Remova um filro para continar</p> : null);
 
   const handleFilter = () => {
@@ -21,44 +22,48 @@ export default function NumericFilter() {
 
   const dataComparison = ['maior que', 'menor que', 'igual a'];
   return (
-    <form>
-      <select
-        data-testid="column-filter"
-        name="column"
-        value={ filterInputs.column }
-        onChange={ handleChange }
-      >
-        {filteredColumns && filteredColumns
-          .map((opt, i) => <option key={ i }>{opt}</option>)}
-      </select>
-      <select
-        data-testid="comparison-filter"
-        name="comparison"
-        value={ filterInputs.comparison }
-        onChange={ handleChange }
-      >
-        {dataComparison.map((opt, i) => <option key={ i }>{opt}</option>)}
-      </select>
-      <label htmlFor="value">
-        <input
-          id="value"
-          name="value"
-          value={ filterInputs.value }
+    <>
+
+      <form>
+        <select
+          data-testid="column-filter"
+          name="column"
+          value={ filterInputs.column }
           onChange={ handleChange }
-          data-testid="value-filter"
-          type="number"
-          placeholder="type a number"
-        />
-      </label>
-      <button
-        data-testid="button-filter"
-        type="button"
-        onClick={ handleFilter }
-        disabled={ alert(!filterInputs.column) }
-      >
-        Filter
-      </button>
-      {alert(!filterInputs.column)}
-    </form>
+        >
+          {filteredColumns && filteredColumns
+            .map((opt, i) => <option key={ i }>{opt}</option>)}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+          value={ filterInputs.comparison }
+          onChange={ handleChange }
+        >
+          {dataComparison.map((opt, i) => <option key={ i }>{opt}</option>)}
+        </select>
+        <label htmlFor="value">
+          <input
+            id="value"
+            name="value"
+            value={ filterInputs.value }
+            onChange={ handleChange }
+            data-testid="value-filter"
+            type="number"
+            placeholder="type a number"
+          />
+        </label>
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ handleFilter }
+          disabled={ alert() }
+        >
+          Filter
+        </button>
+      </form>
+      <OrderFilter />
+      {alert()}
+    </>
   );
 }
