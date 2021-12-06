05ed1261c2b7ae842294b9e71f2eac51481a6c92
Julio <julioaranabr@yahoo.com.br>
2021-12-06 15:34:00 -0300

    Updates

---
+++ precache-manifest.c94f578767ce02e7897852923b6456d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce63c0a3cd8faf9aa2abe48e1e96ca8e",
+    "url": "/Trybe_Wars/index.html"
+  },
+  {
+    "revision": "a543148f9e65386b1c31",
+    "url": "/Trybe_Wars/static/css/main.28503a2d.chunk.css"
+  },
+  {
+    "revision": "387574cf5f3b12e56d2c",
+    "url": "/Trybe_Wars/static/js/2.260e3c05.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Trybe_Wars/static/js/2.260e3c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a543148f9e65386b1c31",
+    "url": "/Trybe_Wars/static/js/main.23f6d3ca.chunk.js"
+  },
+  {
+    "revision": "9ccd806bdcf1247a26dc",
+    "url": "/Trybe_Wars/static/js/runtime-main.826bf62d.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/Trybe_Wars/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trybe-wars/precache-manifest.5271a0b6d853efce7ccf23db869686f8.js"
+  "/Trybe_Wars/precache-manifest.c94f578767ce02e7897852923b6456d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trybe-wars/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Trybe_Wars/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
