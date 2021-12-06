baf86133c04eadb62c8711feb607152117746d62
Julio <julioaranabr@yahoo.com.br>
2021-12-06 12:26:11 -0300

    Updates

---
+++ precache-manifest.dd4be5c23df10eba0b23340fa085d355.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1feb0c2775d798db426dee77a91231",
+    "url": "/trybe-wars/index.html"
+  },
+  {
+    "revision": "7d459aef092d18606113",
+    "url": "/trybe-wars/static/css/main.81ad9b4a.chunk.css"
+  },
+  {
+    "revision": "f0cf9ea78c5becbcd6c0",
+    "url": "/trybe-wars/static/js/2.74c56a4d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/trybe-wars/static/js/2.74c56a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d459aef092d18606113",
+    "url": "/trybe-wars/static/js/main.23f6d3ca.chunk.js"
+  },
+  {
+    "revision": "cffe2df22c53a012b04d",
+    "url": "/trybe-wars/static/js/runtime-main.3335517f.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/trybe-wars/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
